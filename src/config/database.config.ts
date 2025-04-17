5e082de408864199b6d6a89561762ad35dd6c45b
rish2abh <rishabhshrivastava205@gmail.com>
2025-04-17 23:30:37 +0530

    user upload

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  uri: process.env.MONGODB_URI || 'mongodb+srv://bhushanpawar2112001:bdlNJfpzX1I1OA4s@kryupa.6elfh.mongodb.net/hostel_management',
+  // uri: process.env.MONGODB_URI || 'mongodb+srv://bhushanpawar2112001:bdlNJfpzX1I1OA4s@kryupa.6elfh.mongodb.net/hostel_management',
+  uri: process.env.MONGODB_URI || 'mongodb+srv://meetamanghani21:ttcTacHaS7VzRYEB@cluster0.ctfd11e.mongodb.net/hostel_management',
 })); 
